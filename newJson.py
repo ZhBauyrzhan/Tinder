0b4c252df12605fe22bc593e974081ae981b3470
ZhBauyrzhan <bauyrzhanzhonkebayev@gmail.com>
2021-03-27 16:01:34 +0600

    Третий урок(С базами)

--- newJson.py
+++ newJson.py
@@ -1,72 +1,72 @@
 import string 
 import random
-# user = open('user.txt', 'w')
-# user.write(
-#         '[\n'
-#     )
-# for i in range(3):
-#     letters = string.ascii_letters
-#     login = ( ''.join(random.choice(letters) for i in range(10)) )
-#     password = ( ''.join(random.choice(letters) for i in range(15)) )
-#     fname = ( ''.join(random.choice(letters) for i in range(10)) )
-#     lname = ( ''.join(random.choice(letters) for i in range(10)) )
-#     dig = string.digits
-#     ss = random.randint(0,1)
-#     sex = 'male'
-#     if ss == 0:
-#         sex = 'male'
-#     else:
-#         sex = 'female'
-#     country = ( ''.join(random.choice(letters) for i in range(8)) )
-#     city = ( ''.join(random.choice(letters) for i in range(7)) )
-#     birthday = ( ''.join(random.choice(dig) for i in range(8)) )
-#     day = ( ''.join(random.choice(dig) for i in range(8)) )
-#     phone = '+' + ( ''.join(random.choice(dig) for i in range(7)) )
-#     user.write( '{\n' + "\"id\": " + "\""+ str(i)+ "\",\n" + "\"login\": " + "\""+ login + "\",\n" + "\"password\": " + "\""+ password+ "\",\n" + "\"firstName\": " + "\"" + fname + "\",\n"   + "\"lastName\": " + "\"" + lname + "\",\n"  + "\"sex\": " + "\"" + sex + "\",\n"   
-#     + "\"country\": " + "\"" + country + "\",\n" 
-#     + "\"city\": " + "\"" + city + "\",\n"
-#     + "\"birthDay\": " + "\"" + birthday + "\",\n"
-#     + "\"date\": " + "\"" + day + "\",\n"
-#     + "\"phone\": " + "\"" + phone + "\",\n"
-#     + "\"status\": " + "\"" + "common" + "\"\n"
-#     + '},\n')
-# user.write(']')
+user = open('user.txt', 'w')
+user.write(
+        '[\n'
+    )
+for i in range(3):
+    letters = string.ascii_letters
+    login = ( ''.join(random.choice(letters) for i in range(10)) )
+    password = ( ''.join(random.choice(letters) for i in range(15)) )
+    fname = ( ''.join(random.choice(letters) for i in range(10)) )
+    lname = ( ''.join(random.choice(letters) for i in range(10)) )
+    dig = string.digits
+    ss = random.randint(0,1)
+    sex = 'male'
+    if ss == 0:
+        sex = 'male'
+    else:
+        sex = 'female'
+    country = ( ''.join(random.choice(letters) for i in range(8)) )
+    city = ( ''.join(random.choice(letters) for i in range(7)) )
+    birthday = ( ''.join(random.choice(dig) for i in range(8)) )
+    day = ( ''.join(random.choice(dig) for i in range(8)) )
+    phone = '+' + ( ''.join(random.choice(dig) for i in range(7)) )
+    user.write( '{\n' + "\"id\": " + "\""+ str(i)+ "\",\n" + "\"login\": " + "\""+ login + "\",\n" + "\"password\": " + "\""+ password+ "\",\n" + "\"firstName\": " + "\"" + fname + "\",\n"   + "\"lastName\": " + "\"" + lname + "\",\n"  + "\"sex\": " + "\"" + sex + "\",\n"
+    + "\"country\": " + "\"" + country + "\",\n"
+    + "\"city\": " + "\"" + city + "\",\n"
+    + "\"birthDay\": " + "\"" + birthday + "\",\n"
+    + "\"date\": " + "\"" + day + "\",\n"
+    + "\"phone\": " + "\"" + phone + "\",\n"
+    + "\"status\": " + "\"" + "common" + "\"\n"
+    + '},\n')
+user.write(']')
 
-# memef = open('meme.txt', 'w')
-# memef.write(
-#         '[\n'
-#     )
-# for i in range(100):
-#     letters = string.ascii_letters
-#     meme = ( ''.join(random.choice(letters) for i in range(10)) )
-#     dig = string.digits
-#     day = ( ''.join(random.choice(dig) for i in range(8)) )
-#     memef.write( '{\n' + "\"id\": " + "\""+ str(i)+ "\",\n" + "\"link\": " + "\"" + meme + "\",\n"   + "\"date\": " + "\"" + day +"\"\n"+ '},\n')
-# memef.write(']\n')
+memef = open('meme.txt', 'w')
+memef.write(
+        '[\n'
+    )
+for i in range(100):
+    letters = string.ascii_letters
+    meme = ( ''.join(random.choice(letters) for i in range(10)) )
+    dig = string.digits
+    day = ( ''.join(random.choice(dig) for i in range(8)) )
+    memef.write( '{\n' + "\"id\": " + "\""+ str(i)+ "\",\n" + "\"link\": " + "\"" + meme + "\",\n"   + "\"date\": " + "\"" + day +"\"\n"+ '},\n')
+memef.write(']\n')
 
 
-# memeReview = open('memeReview.txt', 'w')
-# memeReview.write(
-#         '[\n'
-#     )
-# for i in range(100):
-#     letters = string.ascii_letters
-#     userId = random.randint(0, 99)
-#     memeId = random.randint(0, 99)
-#     li = random.randint(0,1)
-#     liked = "true"
-#     if li == 0:
-#         liked="false"
-#     dig = string.digits
-#     day = ( ''.join(random.choice(dig) for i in range(8)) )
-#     memeReview.write( '{\n' + 
-#     "\"id\": " + "\""+ str(i)+ "\",\n" 
-#     + "\"userId\": " + "\"" + str(userId) + "\",\n"   
-#     + "\"memeId\": " + "\"" + str(memeId) + "\",\n"
-#     + "\"date\": " + "\"" + day +"\",\n"
-#     + "\"liked\": " + "\"" + str(liked) +"\"\n"
-#     + '},\n')
-# memeReview.write(']\n')
+memeReview = open('memeReview.txt', 'w')
+memeReview.write(
+        '[\n'
+    )
+for i in range(100):
+    letters = string.ascii_letters
+    userId = random.randint(0, 99)
+    memeId = random.randint(0, 99)
+    li = random.randint(0,1)
+    liked = "true"
+    if li == 0:
+        liked="false"
+    dig = string.digits
+    day = ( ''.join(random.choice(dig) for i in range(8)) )
+    memeReview.write( '{\n' +
+    "\"id\": " + "\""+ str(i)+ "\",\n"
+    + "\"userId\": " + "\"" + str(userId) + "\",\n"
+    + "\"memeId\": " + "\"" + str(memeId) + "\",\n"
+    + "\"date\": " + "\"" + day +"\",\n"
+    + "\"liked\": " + "\"" + str(liked) +"\"\n"
+    + '},\n')
+memeReview.write(']\n')
 
 userIntermation = open('userIntermation', 'w')
 userIntermation.write(
